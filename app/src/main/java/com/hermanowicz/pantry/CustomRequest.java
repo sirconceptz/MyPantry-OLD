a2c8c7c90bd7b8c930b222d16046309f984153ee
Mateusz Hermanowicz <gghost92@gmail.com>
2019-03-16 09:13:32 +0100

    Add scan the product functionality
    Add scan the product functionality
    Add polish language support
    Add germany language support
    Add class needed to send email notifications
    
    Signed-off-by: Mateusz Hermanowicz <gghost92@gmail.com>

--- app/src/main/java/com/hermanowicz/pantry/CustomRequest.java
+++ app/src/main/java/com/hermanowicz/pantry/CustomRequest.java
@@ -7,3 +7,55 @@
  ******************************************************************************/
 
 package com.hermanowicz.pantry;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
+import com.android.volley.Response.Listener;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+
+/**
+ * <h1>CustomRequest</h1>
+ * A helper class for mail sending by mail API.
+ *
+ * @author  Mateusz Hermanowicz
+ * @version 1.0
+ * @since   1.0
+ */
+public class CustomRequest extends Request<JSONObject> {
+
+    private Listener<JSONObject> listener;
+
+    public CustomRequest(int method, String url, Map<String, String> params,
+                         Listener<JSONObject> reponseListener, ErrorListener errorListener) {
+        super(method, url, errorListener);
+        this.listener = reponseListener;
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            return Response.success(new JSONObject(jsonString),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+
+    @Override
+    protected void deliverResponse(JSONObject response) {
+        listener.onResponse(response);
+    }
+}
--- app/src/main/java/com/hermanowicz/pantry/ScanProductActivity.java
+++ app/src/main/java/com/hermanowicz/pantry/ScanProductActivity.java
@@ -11,9 +11,21 @@ package com.hermanowicz.pantry;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <h1>ScanProductActivity/h1>
@@ -33,8 +45,56 @@ public class ScanProductActivity extends AppCompatActivity {
         setContentView(R.layout.activity_scan_product);
 
         context = getApplicationContext();
+        IntentIntegrator qrCodeScanner = new IntentIntegrator(this);
+        qrCodeScanner.setPrompt(getResources().getString(R.string.ScanProductActivity_scan_qr_code));
+        qrCodeScanner.setOrientationLocked(true);
+        qrCodeScanner.setBeepEnabled(true);
+        qrCodeScanner.setCameraId(0);
+        qrCodeScanner.initiateScan();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(context, getResources().getString(R.string.ScanProductActivity_product_not_found), Toast.LENGTH_LONG).show();
+                Intent mainActivityIntent = new Intent(context, MainActivity.class);
+                startActivity(mainActivityIntent);
+                finish();
+            }
+            else {
+                startActivity(createProductDetailsIntent(result.getContents()));
+            }
+        }
+        else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private Intent createProductDetailsIntent(String scanResult){
+        Intent productDetailsIntent = new Intent(context, ProductDetailsActivity.class);
+        List <Integer> decodedQRCodeAsList = decodedQRCode(scanResult);
+        productDetailsIntent.putExtra("product_id", decodedQRCodeAsList.get(0));
+        productDetailsIntent.putExtra("hash_code", decodedQRCodeAsList.get(1));
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator != null) {
+            vibrator.vibrate(Const.VIBRATE_DURATION);
+        }
+        return productDetailsIntent;
+    }
+
+    private List<Integer> decodedQRCode (String scanResult){
+        List <Integer> decodedQRCodeAsList = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(scanResult);
+            decodedQRCodeAsList.add(jsonObject.getInt("product_id"));
+            decodedQRCodeAsList.add(jsonObject.getInt("hash_code"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(context, scanResult, Toast.LENGTH_LONG).show();
+        }
+        return decodedQRCodeAsList;
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, @NonNull KeyEvent event) {
